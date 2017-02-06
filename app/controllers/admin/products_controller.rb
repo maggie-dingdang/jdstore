f10401bac34589387d1839013314fc2460bb4b86
maggie <maggie20161225@gmail.com>
2017-02-06 18:23:58 +0800

    if admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :admin_required
     def index
         @products = Product.all
      end
@@ -9,7 +11,7 @@ class Admin::ProductsController < ApplicationController
 
     def edit
         @product = Product.find(params[:id])
- end
+     end
 
     def update
         @product = Product.find(params[:id])
